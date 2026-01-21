65ade0864e1782004dde5f80d8f985817f2ac841
rushwing <ruoxu.wang@gmail.com>
2026-01-21 20:53:39 +0800

    feat(problems): commit the answer to the problem "0206_reverse_linked_list"
    <WHY>
    - Practise reverse linked list
    
    <WHAT>
    - commit the answer to the problem "0206_reverse_linked_list"
    
    <TEST>
    - Gtest

---
+++ tests/test_0206_reverse_linked_list.cpp
@@ -0,0 +1,71 @@
+#include <gtest/gtest.h>
+#include "0206_reverse_linked_list.h"
+#include <vector>
+
+class ReverseLinkedListTest : public ::testing::Test {
+protected:
+    Solution solution;
+
+    ListNode* createList(const std::vector<int>& values) {
+        if (values.empty()) return nullptr;
+        ListNode* head = new ListNode(values[0]);
+        ListNode* current = head;
+        for (size_t i = 1; i < values.size(); i++) {
+            current->next = new ListNode(values[i]);
+            current = current->next;
+        }
+        return head;
+    }
+
+    std::vector<int> toVector(ListNode* head) {
+        std::vector<int> result;
+        while (head) {
+            result.push_back(head->val);
+            head = head->next;
+        }
+        return result;
+    }
+
+    void deleteList(ListNode* head) {
+        while (head) {
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+TEST_F(ReverseLinkedListTest, Example1) {
+    // [1,2,3,4,5] -> [5,4,3,2,1]
+    ListNode* head = createList({1, 2, 3, 4, 5});
+    ListNode* result = solution.reverseList(head);
+    EXPECT_EQ(toVector(result), std::vector<int>({5, 4, 3, 2, 1}));
+    deleteList(result);
+}
+
+TEST_F(ReverseLinkedListTest, Example2) {
+    // [1,2] -> [2,1]
+    ListNode* head = createList({1, 2});
+    ListNode* result = solution.reverseList(head);
+    EXPECT_EQ(toVector(result), std::vector<int>({2, 1}));
+    deleteList(result);
+}
+
+TEST_F(ReverseLinkedListTest, SingleNode) {
+    ListNode* head = createList({1});
+    ListNode* result = solution.reverseList(head);
+    EXPECT_EQ(toVector(result), std::vector<int>({1}));
+    deleteList(result);
+}
+
+TEST_F(ReverseLinkedListTest, Empty) {
+    ListNode* result = solution.reverseList(nullptr);
+    EXPECT_EQ(result, nullptr);
+}
+
+TEST_F(ReverseLinkedListTest, ThreeNodes) {
+    ListNode* head = createList({1, 2, 3});
+    ListNode* result = solution.reverseList(head);
+    EXPECT_EQ(toVector(result), std::vector<int>({3, 2, 1}));
+    deleteList(result);
+}
